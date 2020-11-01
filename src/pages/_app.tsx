a2c3d7e44e0235814e97021f7ff0938c037d1cce
Matheus Azevedo <matheusbzevedo@gmail.com>
2020-11-01 17:00:29 -0300

    add(arquitetura): adiciona env e código do GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,18 @@ import GlobalStyles from './../styles/global';
 import theme from '../styles/theme';
 import Head from 'next/head';
 
+// export function reportWebVitals({ id, name, label, value }) {
+//   // Use `window.gtag` if you initialized Google Analytics as this example:
+//   // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
+//   window.gtag('event', name, {
+//     event_category:
+//       label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+//     value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+//     event_label: id, // id unique to current page load
+//     non_interaction: true, // avoids affecting bounce rate.
+//   })
+// }
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,21 @@ export default class MyDocument extends Document {
           <link rel='icon' href='/favicon.co' />
           <meta name='description' content='eSports organization' />
           <meta name='google-site-verification' content='google-site-verification=2DI1H-9G6B-B16rL0tNIeM0Fj2D-8Uqjmv5hlSduhGs' />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}/>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (window.location.hostname !== 'localhost') {
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                }
+              `,
+            }}
+          />
           <link
             rel='preload'
             href='./fonts/Dolce Vita.ttf'
